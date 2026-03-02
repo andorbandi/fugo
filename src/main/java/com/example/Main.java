02dd08204cc426e153a9169888a80ca63febaaa8
Szép Andor <andorbandi08@gmail.com>
2026-03-02 15:32:04 +0100

    Fájlbaír osztály (Storage) kész

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -13,6 +13,8 @@ package com.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        Sol.task01();
+        // Sol.task01();
+        // Storage.writeToFile(null);
+        
     }
 }
--- src/main/java/com/example/Storage.java
+++ src/main/java/com/example/Storage.java
@@ -1,7 +1,23 @@
 package com.example;
 
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Storage {
-    public static void task01() {
-        System.out.println("Hello world!");
+
+    public static void writeToFile(String content) {
+        // Itt kezeljük a hibát, vagy másképp a kivételt, itt nem dobjuk el: //
+        try {
+            tryWriteToFile(content);
+        } catch (IOException e) {
+            System.err.println("Hiba! A fájl nem írható!");
+            System.err.println("A hiba: " + e.getMessage());
+        }
+    }
+
+    public static void tryWriteToFile(String content) throws IOException {
+        FileWriter fileWriter = new FileWriter("adat.txt");
+        fileWriter.write(content);
+        fileWriter.close();
     }
 }
